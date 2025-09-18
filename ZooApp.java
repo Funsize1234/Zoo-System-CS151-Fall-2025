1f3b230a4c0341f6e32d8ddb82e29c3dc375a4bc
Atay Moya <amoya558@student.fuhsd.org>
2025-09-18 00:05:55 -0700

    added beginning code

---
+++ ZooApp.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ZooApp {
+    private final Scanner sc;
+
+    public ZooApp(Scanner sc) {
+        this.sc = sc;
+    }
+    
+    public void start() {
+        System.out.println("Welcome to the Zoo");
+    }
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        ZooApp app = new ZooApp(sc);
+        app.start();
+    }
+}
