00238bf0949a70be80ad2ddac0da043637428501
bvlh <brandonh15527@gmail.com>
2025-09-26 22:51:22 -0700

    Update Runnable.java

--- src/main/java/animals/Runnable.java
+++ src/main/java/animals/Runnable.java
@@ -4,5 +4,18 @@ public interface Runnable {
 
     public void run();
 
+<<<<<<< Updated upstream
     public int getTop();
+=======
+    public void stop();
+
+    public int getTopSpeed();
+
+    public void jump();
+
+    public boolean isRunning();
+    
+    public int  getJumpCount();
+    
+>>>>>>> Stashed changes
 }
