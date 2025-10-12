dd9f3ac52e821ccacc79c53869eb2d0ba2bc11c3
Isaac Fu <113050907+FuIsaac@users.noreply.github.com>
2025-10-11 19:55:10 -0700

    added unique print statements for each animal trick

--- src/main/java/animals/Tortoise.java
+++ src/main/java/animals/Tortoise.java
@@ -15,6 +15,7 @@ public class Tortoise extends ZooAnimal implements Runnable {
     
     @Override
     public int performGroundTricks() {
+        System.out.println(name + " slowly crawls around, drawing the attention of more visitors");
         return getTrickPoints();
     }
     
