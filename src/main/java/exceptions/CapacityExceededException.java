e8d6fd9bb753a6a80308a196980ea42adfdf3b60
funsize1324 <cjlou45@icloud.com>
2025-10-12 02:04:37 -0700

    deleted capacityexceededexception and made animalnotfoundexception more specific

--- src/main/java/exceptions/CapacityExceededException.java
+++
@@ -1,9 +0,0 @@
-package src.main.java.exceptions;
-
-public class CapacityExceededException extends Exception {
-
-    public CapacityExceededException(int capacity) {
-        super("Capacity Exceeded: Max " + capacity + " can be stored");
-    }
-    
-}
