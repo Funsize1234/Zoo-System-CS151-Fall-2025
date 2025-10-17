daeb4cf7a850f85f2142a58ece4a7c5147925582
Hazelette <62589987+Hazelette@users.noreply.github.com>
2025-10-16 19:50:23 -0700

    feat: tuesday discount of 20% to ticket price

--- src/main/java/zoo/Ticket.java
+++ src/main/java/zoo/Ticket.java
@@ -14,5 +14,13 @@ public class Ticket {
     private static final double ADULT_PRICE  = 20.00;
     private static final double CHILD_PRICE  = 12.00;
     private static final double SENIOR_PRICE = 15.00;
+    private static final double TUESDAY_DISCOUNT = 0.2;
+
+    private Ticket() {}
+
+    private static boolean isTuesday(LocalDate date){
+        if (date == null) date = LocalDate.now();
+        return date.getDayOfWeek() == DayOfWeek.TUESDAY; 
+
 
 }
