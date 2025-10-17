1bc2b12e1b8956d4114d8d270012d1127e1d64bc
Hazelette <62589987+Hazelette@users.noreply.github.com>
2025-10-16 19:47:13 -0700

    Moved enum into a singular Ticket class for modularity

---
+++ src/main/java/zoo/Ticket.java
@@ -0,0 +1,18 @@
+package src.main.java.zoo;
+
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+
+
+public class Ticket {
+    public enum TicketType {
+        ADULT, CHILD, SENIOR
+    }
+
+    private static final double ADULT_PRICE  = 20.00;
+    private static final double CHILD_PRICE  = 12.00;
+    private static final double SENIOR_PRICE = 15.00;
+
+}
